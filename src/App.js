15a94307d1a5345df203592cb91833483ee54999
Ian Miller 🦐 <ianmiller347@gmail.com>
2020-12-17 19:03:13 -0500

    add josh by joseph init page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Josh by Joseph Carr Wine</h1>
+        <p>Josh wines by my dad Joseph, but his friends call him Josh.</p>
+        <p>Explore our delicious wines.</p>
       </header>
     </div>
   );
