df8ace671304778eed39cf1830d07f02e188fc9a
Ian Miller 🦐 <ianmiller347@gmail.com>
2020-12-17 21:26:50 -0500

    update readme, site description, and homepage text

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Josh by Joseph Carr Wine</h1>
-        <p>Josh wines by my dad Joseph, but his friends called him Josh.</p>
-        <p>Explore our delicious wines.</p>
-        <p>This Cabernet is a classic!</p>
+        <p>Josh wines is named after Joseph's dad. His dad's name was Joseph, but his friends called him Josh.</p>
+        <p>This Cabernet is a classic! In the beginning, he only made one wine, Cabernet Sauvignon, but soon thereafter began making a Josh Chardonnay for his wife, Dee.</p>
         <div>
           <Image 
             src="https://i.ibb.co/2v97GNd/josh-wine.jpg"
